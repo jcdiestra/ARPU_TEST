9941a2f809b46cb3e92c21827bff5b5ca62a5278
Juancass <jcdiestra@gmail.com>
2017-03-23 10:52:17 -0500

    Cambio en conexiones db

--- db_connection.php
+++ db_connection.php
@@ -1,7 +1,9 @@
 <?php
 
-$link = mysql_connect('192.168.0.12', 'admin', 'lamarksql')
-   or die('Could not connect: ' . mysql_error());
+$link = mysql_connect('localhost', 'root', '')
+   or die('Could not connect: ' . mysql_error()); // Conexión Juancass
+/*$link = mysql_connect('localhost', 'admin', 'lamarksql')
+   or die('Could not connect: ' . mysql_error()); // Conexión Diego*/
      
 mysql_select_db('arpu_report') or die('Could not select database');
 
